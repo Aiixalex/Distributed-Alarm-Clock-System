f8daf5656345c157e0669989dcc9d2325caabf13
Henry <henry@ubuntuVM.com>
2022-04-07 14:36:54 -0700

    update keypad module

--- src/keypad/keypad.c
+++ src/keypad/keypad.c
@@ -1,22 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include "utility.h"
 
 #define GPIO_BASE_DIR "/sys/class/gpio"
 #define NUM_KEYPAD_GPIO 12
 #define MAX_BUFFER_SIZE 256
 #define SLEEP_TIME_MS 100
+#define MAX_READ_LENGTH 5
 
 static const char *KEYPAD_GPIO[NUM_KEYPAD_GPIO] = {
     "88", "89", "86", "87", "10", "9", "8", "78", "76", "74", "72", "70"
 };
+static const int KEYPAD_KEYS[NUM_KEYPAD_GPIO] = {
+    -1, 7, 4, 1, 0, 8, 5, 2, -1, 9, 6, 3
+}
 
-typedef enum keypad_keys {
-    star, seven, four, one, zero, eight, give, two, hash, nine, six, three
+static bool keypad_isResetted[NUM_KEYPAD_GPIO] = { false };
+static char userInput[MAX_READ_LENGTH + 1] = "";
+static int userInputIndex = 0;
+
+typedef enum keypad_element {
+    star, seven, four, one, zero, eight, five, two, hash, nine, six, three
 } keypad_element;
-
-
 
 void Keypad_init(void) {
     for (int i = 0; i < NUM_KEYPAD_GPIO; i++) {
@@ -29,4 +36,26 @@ void Keypad_init(void) {
     }
 
     my_sleep_ms(SLEEP_TIME_MS);
+}
+
+int Keypad_readUserInput(void) {
+    char userInput[MAX_READ_LENGTH + 1] = "";
+    int userInputIndex = 0;
+
+    while (true) {
+        for (int i = 0; i < NUM_KEYPAD_GPIO; i++) {
+            char tempString[MAX_BUFFER_SIZE];
+            snprintf(tempString, 2, "/sys/class/gpio/gpio%s/value", KEYPAD_GPIO[i]);
+
+            int isPressed = file_read_int(tempString);
+            if (KEYPAD_KEYS[i] == -1) {
+                userInput[MAX_READ_LENGTH] = '\0';
+                return atoi(userInput);
+            }
+            else {
+                userInput[userInputIndex % MAX_READ_LENGTH] = KEYPAD_KEYS[i];
+                userInputIndex++;
+            }            
+        }
+    }
 }
