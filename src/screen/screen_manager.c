f0fad5bad9652408b04cdea3d689e7330fc4d15d
Piercson <piie1996@googlemail.com>
2022-04-08 23:56:37 -0700

    Finished core Screen manager

--- src/screen/screen_manager.c
+++ src/screen/screen_manager.c
@@ -1,32 +1,125 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "screen_manager.h" 
 #include "OLED_text.h"
 
-void screen_init(void);
-void screen_destroy(void);
-// Setters
-void screen_setInverseDisplay(void);
-void screen_text_setTextXY(unsigned char Row, unsigned char Column);
-void screen_setContrastLevel(unsigned char ContrastLevel);
-void screen_putString(const char* String);
-void screen_setHorizontalScrollProperties(bool direction, unsigned char startRow, unsigned char endRow,
-                            unsigned char startColumn, unsigned char endColumn, unsigned char scrollSpeed);
-void screen_activateScroll(void);
-void screen_deactivateScroll(void);
+#define MAX_CHAR_PER_LINE 12
+
+#define VARIABLE_START_LINE 0
+#define PROBLEM_START_LINE  HOW_TO_START_LINE+1
+
+#define HOW_TO_START_LINE 4
+#define END_LINE  11
+#define DEBUG 1
+
+#define CHAR_HEIGHT 8
+#define SCREEN_ROW_START 8
+#define SCREEN_ROW_END 96
+
+static void set_problem(char* problem, int amt);
+static void set_variables(puzzle problem);
+
 // Init
-void screen_init(void)
+void Screen_init(void)
 {
+    if(DEBUG)
+        printf("Screen init\n");
     OLED_text_init(SSD1327);
+    OLED_text_deactivateScroll();
     OLED_text_clearDisplay();
     OLED_text_setVerticalMode();
 }
 //Destroy
-void screen_destroy(void)
+void Screen_destroy(void)
 {
     OLED_text_clearDisplay();
     OLED_text_destroy();
+}
+
+void Screen_set_problem(puzzle data){
+    OLED_text_clearDisplay();
+    OLED_text_deactivateScroll();
+    switch (data.diffculityLevel){
+    case 1:
+        if(DEBUG)
+            printf("Difficulty 1\n");
+        set_problem(data.problem, 1);
+        break;
+    case 2:
+        if(DEBUG)
+            printf("Difficulty 2\n");
+        set_problem(data.problem, 2);
+        break;
+    case 3:
+        if(DEBUG)
+            printf("Difficulty 3\n");
+        set_problem(data.problem, 3);
+        break;
+    case 4:
+        if(DEBUG)
+            printf("Difficulty 4\n");
+        set_variables(data);
+        set_problem(data.problem, 2);
+        OLED_text_setHorizontalScrollProperties(Scroll_Left, 0,
+                    SCREEN_ROW_START + (CHAR_HEIGHT * 3), 0, 47,
+                    Scroll_2Frames);
+        OLED_text_activateScroll();
+        break;
+    
+    default:
+        break;
+    }
+
+    OLED_text_setTextXY(END_LINE-1,0);
+    OLED_text_putString("Use Integer");
+    OLED_text_setTextXY(END_LINE,0);
+    OLED_text_putString(" Division :)");
+}
+static void set_problem(char* problem, int amt){
+    OLED_text_setTextXY(HOW_TO_START_LINE,0);
+    OLED_text_putString("Solve->Peace");
+    int problem_i = 0;
+    for (int i = 0; i < amt; i++){
+        OLED_text_setTextXY(PROBLEM_START_LINE+i,0);
+        char line[MAX_CHAR_PER_LINE + 1];
+        int line_i = 0;
+        if(i == 0){
+            line[0] = ' ';
+            line[1] = ' ';
+            line_i = 2;
+        }
+        char c;
+        do{
+            c = problem[problem_i];
+            line[line_i] = c;
+            line_i++;
+            problem_i++;
+            if(c == ')'){
+                problem_i++;
+                line[line_i] = 0;
+                break;
+            }
+        }while(c != 0);
+        OLED_text_putString(line);
+    }
+    
+}
+static void set_variables(puzzle problem){
+    OLED_text_setTextXY(VARIABLE_START_LINE,0);
+    char line1[MAX_CHAR_PER_LINE+1] = "";
+    strncat(line1,problem.varOne, 5);
+    strncat(line1, "   ",1);
+    strncat(line1, problem.varTwo, 5);
+    OLED_text_putString(line1);
+
+    OLED_text_setTextXY(VARIABLE_START_LINE+1,0);
+    char line2[MAX_CHAR_PER_LINE+1] = "";
+    strncat(line2,problem.varThree, 5);
+    strncat(line2, "   ",1);
+    strncat(line2, problem.varFour, 5);
+    OLED_text_putString(line2);
 }
 
 
--- src/screen/screen_manager.h
+++ src/screen/screen_manager.h
@@ -2,17 +2,26 @@
 #define SCREEN_H_
 #include <stdbool.h>
 #include "OLED_text.h"
+
+#define MAX_STRING_LENGTH 50
+
+typedef enum diffculity {
+    easy = 1, medium = 2, hard = 3, genius
+} diffculity;
+
+typedef struct puzzle {
+    diffculity diffculityLevel;
+    char problem[MAX_STRING_LENGTH];
+    char varOne[MAX_STRING_LENGTH];
+    char varTwo[MAX_STRING_LENGTH];
+    char varThree[MAX_STRING_LENGTH];
+    char varFour[MAX_STRING_LENGTH];
+    int answer;
+} puzzle;
 // Initilizers and Destructors
-void screen_init(void);
-void screen_destroy(void);
+void Screen_init(void);
+void Screen_destroy(void);
 // Setters
-void screen_setInverseDisplay(void);
-void screen_text_setTextXY(unsigned char Row, unsigned char Column);
-void screen_setContrastLevel(unsigned char ContrastLevel);
-void screen_putString(const char* String);
-void screen_setHorizontalScrollProperties(bool direction, unsigned char startRow, unsigned char endRow,
-                            unsigned char startColumn, unsigned char endColumn, unsigned char scrollSpeed);
-void screen_activateScroll(void);
-void screen_deactivateScroll(void);
+void Screen_set_problem(puzzle data);
 
 #endif
